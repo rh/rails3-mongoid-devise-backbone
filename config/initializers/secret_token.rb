c5e1dfea8e01b73e5f3effebba30ca9cacb811a6
Richard Hubers <richard.hubers@gmail.com>
2012-08-28 10:04:13 +0200

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NotesApp::Application.config.secret_token = '0bcff02476f96870631362997594f298b6501ed6770a8d7bc4dc78fc3b7b02b28109dd28d2875290ad9bf19c4d95b013deba6ab2fd2d57fe8f25c87ecec67094'
