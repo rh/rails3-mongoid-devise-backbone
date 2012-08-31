aaa88369a47a8a4a83bedf67fd0e33496eb67e59
Richard Hubers <richard.hubers@gmail.com>
2012-08-31 13:19:28 +0200

    Removed console.dir call

--- app/assets/javascripts/views/note.js
+++ app/assets/javascripts/views/note.js
@@ -10,7 +10,6 @@ var NoteView = Backbone.View.extend({
   render: function() {
     var date = moment(this.model.get('created_at')).format('DD-MM-YYYY HH:mm');
     var context = _.extend({}, this.model.toJSON(), {date: date});
-    console.dir(context);
     var html = this.template(context);
     this.$el.html(html);
 
