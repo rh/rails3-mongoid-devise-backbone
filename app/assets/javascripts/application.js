905d7a569389b5bc059fb3fcc0956779b9f8d7ad
Richard Hubers <richard.hubers@gmail.com>
2012-09-04 21:53:03 +0200

    Remove Moment.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require moment
 //= require underscore
 //= require backbone
 //= require handlebars
--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,9 +1,20 @@
 var Date = Date || {};
 
+// Expects a string in the format '2012-09-04T21:10:14+02:00'
+// and returns a string in the format '04-09-2012 21:10'
 Date.format = function(date) {
   if (date) {
-    return moment(date).format('DD-MM-YYYY HH:mm');
+    var parts = date.split('T');
+
+    if (parts.length == 2) {
+      var d = parts[0].split('-');
+
+      if (d.length == 3 && parts[1].length >= 5) {
+        return [d[2], '-', d[1], '-', d[0], ' ', parts[1].substring(0, 5)].join('');
+      }
+    }
   }
+
   return '';
 };
 
